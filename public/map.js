d42a0e56545bcc7d28e3c655e5b9491ecd56aa51
Jose M Rico <jrico@princeton.edu>
2017-06-27 00:56:30 -0400

    sidebar

--- public/map.js
+++ public/map.js
@@ -1,11 +1,32 @@
+var $sidebar;
+var $main;
+var $toggleSidebar;
+
+function toggleSidebar() {
+    if ($sidebar.width() === 0) {
+        $sidebar.width(250);
+        $main.css("margin-left", "250px");
+        $toggleSidebar.css("margin-left", "250px");
+    }
+    else {
+        $sidebar.width(0);
+        $main.css("margin-left", "0");
+        $toggleSidebar.css("margin-left", "0");
+    }
+}
+
 window.onload = function() {
-    var mymap = L.map("map", {
+    var myMap = L.map("map", {
         center: [40.3440774, -74.6581347],
         zoom: 16,
         zoomSnap: 0.25,
-        zoomDelta: 0.5
+        zoomDelta: 0.5,
+        maxBounds: [
+            [40.3062834,-74.6837298],
+            [40.3615089,-74.6441935]
+        ],
+        zoomControl: false // add manually later to top right
     });
-    //}).setView([40.3440774, -74.6581347], 16);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -13,8 +34,15 @@ window.onload = function() {
         minZoom: 16,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1Ijoiam1yaWNvMDEiLCJhIjoiY2o0MjZvYXZzMDNxeTMzbXphajQ2YmdoayJ9.r5KOkm5E2W9c6o854dXhfw'
-    }).addTo(mymap);
+    }).addTo(myMap);
 
-    var marker = L.marker([40.3440774, -74.6581347]).addTo(mymap);
+    L.control.zoom({position: "topright"}).addTo(myMap);
+
+    var marker = L.marker([40.3440774, -74.6581347]).addTo(myMap);
     marker.bindPopup("<b>Princeton University</b><br>Test");
+
+    $sidebar = $("#sidebar");
+    $main = $("#main");
+    $toggleSidebar = $("#toggleSidebar");
+    $("#toggleSidebarButton").click(toggleSidebar);
 };
