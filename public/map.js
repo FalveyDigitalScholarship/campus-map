1bcdd80d122ab5f2083b62d964e8114811b6aada
Jose M Rico <jrico@princeton.edu>
2017-08-09 20:39:44 -0400

    Humongous changes

--- public/map.js
+++ public/map.js
@@ -1,14 +1,26 @@
 var myMap = null;
-var selectedPolygon = null;
+var locationData = null;
+
+var polygonSelected = null;
+var popupSelected = null;
+var popupHover = null;
+
+var polygonRecentClick = false;
+// Amount of time the recent click flag will be true.
+var polygonRecentClickDuration = 150;
+// Amount of time the map click event waits to confirm no polygon was clicked.
+var polygonRecentClickTime = 50;
 
 var sidebarOpen = false;
+
+var startSelection = "Nassau Hall";
     
 var colors = {
-    "idle": "#444444",
     "purple": "#C6ACC7",
     "red": "#ECB4BF",
     "orange": "#FBD7B7",
-    "blue": "#C2E3EC"
+    "blue": "#C2E3EC",
+    "none": "#FFFFFF"
 };
 var categories = {
     "purple": "Dedicated to slave owner",
@@ -17,7 +29,59 @@ var categories = {
     "blue": "Donated by someone with ties to slavery"
 };
 
-function sidebarButtonTooltipVisible(flag) {
+var popupContent = "<div class='bldgPopup'><b>{0}</b></div>";
+var popupContentSites = "<div class='bldgPopup'><b>{0}</b><br>{1}</div>";
+
+var styleIdle = {
+    weight: 2,
+    opacity: 0.75,
+    fillOpacity: 0.25
+};
+var styleHover = {
+    weight: 2,
+    opacity: 1.0,
+    fillOpacity: 0.6
+};
+var styleSelected = {
+    weight: 2,
+    opacity: 1.0,
+    fillOpacity: 0.6
+};
+
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+            return (typeof args[number] != 'undefined') ? args[number] : match;
+        });
+    };
+}
+
+function IsMobile() {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function SidebarWidth() {
+    return $("#sidebar").width();
+}
+
+function RecenterWithSidebar(latLng) {
+    var targetPoint = myMap.project(latLng, myMap.zoom).subtract([SidebarWidth() / 2, 0]);
+    return myMap.unproject(targetPoint, myMap.zoom);
+}
+
+function SidebarButtonTooltipVisible(flag) {
     var $toggleSidebarTooltip = $("#toggleSidebarTooltip");
 
     if (flag) {
@@ -28,104 +92,168 @@ function sidebarButtonTooltipVisible(flag) {
     }
 }
 
-function toggleSidebar() {
+function ToggleSidebar() {
     var $sidebar = $("#sidebar");
     var $toggleSidebar = $("#toggleSidebar");
     var $toggleSidebarArrow = $("#toggleSidebarArrow");
     var $toggleSidebarTooltip = $("#toggleSidebarTooltip");
 
-    sidebarButtonTooltipVisible(false);
+    SidebarButtonTooltipVisible(false);
 
     if (sidebarOpen) {
-        console.log("open");
-        $sidebar.css("left", "-400px");
+        $sidebar.css("left", "-" + SidebarWidth() + "px");
         $toggleSidebar.css("margin-left", "0");
         $toggleSidebarArrow.css("margin-left", "9px");
         $toggleSidebarArrow.css("border-color", "transparent transparent transparent black");
         $toggleSidebarTooltip.html("Show side panel");
-
-        if (selectedPolygon !== null) {
-            selectedPolygon.setStyle({
-                color: colors["idle"]
-            });
-        }
     }
     else {
-        console.log("close");
         $sidebar.css("left", "0px");
-        $toggleSidebar.css("margin-left", "400px");
+        $toggleSidebar.css("margin-left", SidebarWidth() + "px");
         $toggleSidebarArrow.css("margin-left", "1px");
         $toggleSidebarArrow.css("border-color", "transparent black transparent transparent");
         $toggleSidebarTooltip.html("Hide side panel");
-
-        if (selectedPolygon !== null) {
-            selectedPolygon.setStyle({
-                color: colors[selectedPolygon.options.category]
-            });
-        }
     }
     sidebarOpen = !sidebarOpen;
 }
 
-function onClickBldg(event) {
-    var polygon = event.target;
-    var bldgName = polygon.options.name;
-    var bldgColor = colors[polygon.options.category];
-    var bldgImgPath = "images/" + polygon.options.image;
+function GetPopupFromPolygon(polygon) {
+    var name = polygon.options.name;
+    var subsites = polygon.options.subsites;
 
-    $("#bldgImg").attr("src", bldgImgPath);
-    $("#bldgNameDiv").css("background-color", bldgColor);
-    $("#bldgName").html(bldgName);
-    $("#bldgCategory").html(categories[polygon.options.category]);
+    var latLng = [polygon.getBounds().getNorth(), polygon.getCenter().lng];
 
-    if (selectedPolygon !== null) {
-        selectedPolygon.setStyle({
-            color: colors["idle"]
-        });
+    var content = popupContent.format(name);
+    if (subsites != null) {
+        if (subsites.length == 1) {
+            content = popupContentSites.format(name, "1 site");
+        }
+        else {
+            content = popupContentSites.format(name, subsites.length + " sites");
+        }
     }
-    polygon.setStyle({
-        color: bldgColor
-    });
-    selectedPolygon = polygon;
+
+    var popup = L.popup({
+        autoPan: true,
+        autoClose: false,
+        closeOnClick: false,
+        closeButton: false
+    })
+    .setLatLng(latLng)
+    .setContent(content)
+    .openOn(myMap)
+    .bringToBack();
+
+    return popup;
+}
+
+function DisplayBuildingInfo(info) {
+    var name = info.name;
+    var color = colors[info.category];
+
+    if (info.category === "none") {
+        $("#bldgImg").hide();
+        $("#bldgCategory").hide();
+    }
+    else {
+        $("#bldgCategory").show();
+        $("#bldgCategory").html(categories[info.category]);
+        $("#bldgImg").show();
+        $("#bldgImg").attr("src", "images/" + info.image);
+    }
+    $("#bldgInfo").css("background-color", color);
+    $("#toggleSidebarButton").css("background-color", color);
+    $("#bldgName").html(name);
+}
+
+function SelectPolygon(polygon) {
+    // Deselect polygon and remove popup.
+    if (polygonSelected !== null) {
+        polygonSelected.setStyle(styleIdle);
+    }
+    polygonSelected = polygon;
+    if (popupSelected !== null) {
+        popupSelected.remove();
+    }
+
+    // Display new polygon info if it's not null.
+    if (polygon !== null) {
+        $("#toggleSidebarButton").show();
+        DisplayBuildingInfo(polygon.options);
+
+        polygon.setStyle(styleSelected);
+        popupSelected = GetPopupFromPolygon(polygon);
+    }
+    else {
+        $("#toggleSidebarButton").hide();
+    }
+}
+
+function OnClickBldg(event) {
+    polygonRecentClick = true;
+
+    var polygon = event.target;
+    myMap.panTo(RecenterWithSidebar(polygon.getCenter()));
+
+    SelectPolygon(polygon);
 
     if (!sidebarOpen) {
-        toggleSidebar();
+        ToggleSidebar();
+    }
+
+    setTimeout(function() {
+        polygonRecentClick = false;
+    }, polygonRecentClickDuration);
+}
+function OnClickMap() {
+    // "True" map click.
+    if (sidebarOpen) {
+        ToggleSidebar();
+    }
+
+    // Remove polygon selection.
+    SelectPolygon(null);
+}
+function OnClickMapEvent(event) {
+    // First check if a polygon has also been clicked (event propagates).
+    setTimeout(function() {
+        if (!polygonRecentClick) {
+            // If no polygon has been clicked, we're sure it's the map.
+            OnClickMap();
+        }
+    }, polygonRecentClickTime);
+}
+function OnMouseEnterBldg(event) {
+    var polygon = event.target;
+
+    if (popupHover !== null) {
+        popupHover.remove();
+    }
+    popupHover = GetPopupFromPolygon(polygon);
+
+    polygon.setStyle(styleHover);
+}
+function OnMouseExitBldg(event) {
+    var polygon = event.target;
+
+    if (popupHover != null) {
+        popupHover.remove();
+    }
+    if (polygon !== polygonSelected) {
+        polygon.setStyle(styleIdle);
     }
 }
-function onClickMap(event) {
-    /*console.log("map click");
 
-    if (isSidebarOpen()) {
-        toggleSidebar();
+$(function() {
+    /*if (isMobile()) {
+        $(".desktop").hide();
     }*/
-}
-function onMouseEnterBldg(event) {
-    var polygon = event.target;
-    var name = polygon.options.name;
 
-    $("#bldgNameHover").html("<b>" + name + "</b>");
-    polygon.setStyle({
-        color: colors[polygon.options.category]
-    });
-}
-function onMouseExitBldg(event) {
-    var polygon = event.target;
-
-    $("#bldgNameHover").html("");
-
-    if (polygon !== selectedPolygon || !sidebarOpen) {
-        polygon.setStyle({
-            color: colors["idle"]
-        });
-    }
-}
-
-window.onload = function() {
     myMap = L.map("map", {
         center: [40.3440774, -74.6581347],
-        zoom: 16.5,
+        zoom: 17,
         zoomSnap: 0.25,
-        zoomDelta: 0.5,
+        zoomDelta: 0.25,
         maxBounds: [
             [40.3062834,-74.6837298],
             [40.3615089,-74.6441935]
@@ -133,43 +261,87 @@ window.onload = function() {
         zoomControl: false // add manually later to top right
     });
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    L.tileLayer("https://api.mapbox.com/{style}/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 20,
         minZoom: 16,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoiam1yaWNvMDEiLCJhIjoiY2o0MjZvYXZzMDNxeTMzbXphajQ2YmdoayJ9.r5KOkm5E2W9c6o854dXhfw'
+        //style: "styles/v1/mapbox/dark-v9",
+        style: "styles/v1/jmrico01/cj64oe3nq5ibq2rr8tprzvy56",
+        accessToken: "pk.eyJ1Ijoiam1yaWNvMDEiLCJhIjoiY2o0MjZvYXZzMDNxeTMzbXphajQ2YmdoayJ9.r5KOkm5E2W9c6o854dXhfw"
     }).addTo(myMap);
-
+    
     L.control.zoom({position: "topright"}).addTo(myMap);
 
-    $("#toggleSidebarButton").click(toggleSidebar);
+    $("#toggleSidebarButton").click(ToggleSidebar);
     $("#toggleSidebarButton").hover(function() {
-        sidebarButtonTooltipVisible(true); // Hover in
+        SidebarButtonTooltipVisible(true); // Hover in
     }, function() {
-        sidebarButtonTooltipVisible(false); // Hover out
+        SidebarButtonTooltipVisible(false); // Hover out
     });
+    $("#toggleSidebarButton").hide();
 
-    for (var i = 0; i < locations.length; i++) {
-        var bldgName = locations[i]["name"];
-        for (var j = 0; j < locationData["individual"].length; j++) {
-            if (bldgName === locationData["individual"][j]["name"]) {
-                var polygon = L.polygon(locations[i]["coords"], {
-                    name: bldgName,
-                    category: locationData["individual"][j]["category"],
-                    image: locationData["individual"][j]["image"],
-                    color: colors["idle"],
-                    weight: 2,
-                    fillOpacity: 0.25,
-                    bubblingMouseEvents: false
-                }).addTo(myMap);
-                
-                myMap.on("click", onClickMap);
-                polygon.on("click", onClickBldg);
-                polygon.on("mouseover", onMouseEnterBldg);
-                polygon.on("mouseout", onMouseExitBldg);
-            }
+    $.ajax({
+        dataType: "json",
+        url: "/locationData.json",
+        success: function(data, textStatus, jqXHR) {
+            locationData = data;
+
+            $.ajax({
+                dataType: "json",
+                url: "/locationCoords.json",
+                success: function(coords, textStatus, jqXHR) {
+                    for (var i = 0; i < locationData.length; i++) {
+                        var locName = locationData[i]["name"];
+
+                        if (!(locName in coords))
+                            continue;
+
+                        var color = colors[locationData[i]["category"]];
+                        if (locationData[i]["category"] == "none"
+                        && locationData[i]["subsites"].length == 1) {
+                            color = colors[locationData[i]["subsites"][0]["category"]];
+                        }
+                        var image = locationData[i]["image"];
+                        if (image === null || image === undefined) {
+                            image = "nassau.jpg";
+                        }
+                        var subsites = null;
+                        if ("subsites" in locationData[i]) {
+                            subsites = locationData[i]["subsites"];
+                        }
+
+                        var polygon = L.polygon(coords[locName], {
+                            name: locName,
+                            category: locationData[i]["category"],
+                            image: image,
+                            color: color,
+                            subsites: subsites,
+                            bubblingMouseEvents: false
+                        }).addTo(myMap);
+                        
+                        polygon.setStyle(styleIdle);
+                        
+                        polygon.on("click", OnClickBldg);
+                        polygon.on("mouseover", OnMouseEnterBldg);
+                        polygon.on("mouseout", OnMouseExitBldg);
+
+                        if (locName === startSelection) {
+                            // TODO fix panning, take into account sidebar width
+                            myMap.panTo(RecenterWithSidebar(polygon.getCenter()), {
+                                animate: false
+                            });
+
+                            SelectPolygon(polygon);
+
+                            if (!sidebarOpen) {
+                                ToggleSidebar();
+                            }
+                        }
+                    }
+
+                    myMap.on("click", OnClickMapEvent);
+                }
+            });
         }
-
-    }
-};
+    });
+});
