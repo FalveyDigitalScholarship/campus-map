a64a126a30f6608375002fde6d36ad0d687c94a9
Jose M Rico <jrico@princeton.edu>
2017-08-10 00:08:48 -0400

    Heroku deploy fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + process.argv[2]));//'/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function() {
     console.log("App listening on port " + port);
